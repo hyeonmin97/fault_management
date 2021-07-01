5cbee6127a8020c5f46588bd7278be22af7476b6
DESKTOP-NROS45J\dev_1 <hyeonmin97@naver.com>
2021-07-01 09:11:32 +0900

    modify Test.java
    getter, setter 삭제 -> lombok의 @Data사용

--- src/main/java/com/example/demo/Jpatest/domain/Test.java
+++ src/main/java/com/example/demo/Jpatest/domain/Test.java
@@ -3,26 +3,12 @@ package com.example.demo.Jpatest.domain;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
+@Data
 public class Test {
 	private @Id String id;
 	private String email;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	
 	
 }
