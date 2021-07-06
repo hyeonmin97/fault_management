b5f714f140be4835e3323dc73eaaf91cef15e95b
soyeon134340 <zz3424@naver.com>
2021-07-06 14:46:27 +0900

    스케줄러 슬랙 연동 test

--- src/main/java/com/example/demo/scheduler/Scheduler.java
+++ src/main/java/com/example/demo/scheduler/Scheduler.java
@@ -2,18 +2,43 @@ package com.example.demo.scheduler;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@RestController
 public class Scheduler {
+	
+	@GetMapping("/send")
+    public void send(){
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String,Object> request = new HashMap<String,Object>();
+        request.put("username", "slackbot");
+        request.put("text", "custom-slack-msg");
+
+        HttpEntity<Map<String,Object>> entity = new HttpEntity<Map<String,Object>>(request);
+                    
+        String url = "https://hooks.slack.com/services/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"; // 사용할 슬랙의 Webhook URL 넣기
+
+        restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+    }
 	
     @Scheduled(cron = "0/10 * * * * ?") // 10초마다 확인 
     public void scronJobSch() {
+    	send();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         Date now = new Date();
         String strDate = sdf.format(now);
