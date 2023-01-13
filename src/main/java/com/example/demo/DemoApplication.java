871e1bb1e8842324804972838ba815ccf4872506
hyeonmin <hyeonmin97@naver.com>
2023-01-13 19:15:46 +0900

    Fix: delete unnecessary annotations in DemoApplication
    사용하지 않는 애노테이션 삭제

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,15 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-  
-  @SpringBootApplication
-  
-  @EnableScheduling
-  @EnableJpaAuditing // basetimeentity 이용해 날짜 추가 가능해짐
-  public class DemoApplication {
-  
-  public static void main(String[] args) {
-  SpringApplication.run(DemoApplication.class, args); }
-  
-  }
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
  
