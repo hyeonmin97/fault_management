8966381b0a7178e27d350fba0d0f4665caeba258
hyeonmin <hyeonmin97@naver.com>
2023-01-13 19:10:01 +0900

    Remove: delete existing code
    이전 코드 모두 삭제

--- src/main/java/com/example/demo/repository/querydsl/EngineerList/CustomizedEngineerListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository.querydsl.EngineerList;
-
-import com.example.demo.dto.UpdateDto;
-
-public interface CustomizedEngineerListRepository {
-	public void update(UpdateDto updateDto);
-	public void increaseAge(int value);
-}
