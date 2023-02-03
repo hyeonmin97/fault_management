5500d7e5d9b5dbf66fe422868d0ef900378dceb1
hyeonmin <hyeonmin97@naver.com>
2023-02-03 19:06:11 +0900

    [#1] Feat: add incident Registration page
    장애 등록 페이지 조회 기능 추가

--- src/main/java/com/example/demo/repository/IncidentTypeComponent.java
+++ src/main/java/com/example/demo/repository/IncidentTypeComponent.java
@@ -1,28 +1,44 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.IncidentType;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
-import java.util.Collections;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 
 @Component
 @Transactional(readOnly = true)
 public class IncidentTypeComponent {
     private EntityManager em;
     private List<IncidentType> list;
-    private List<IncidentType> incidentTypeList;
+    //Map<대분류, Map<중분류,List<JSONObject>>
+    //대분류, 중분류는 같고 소분류 리스트에 소분류를 추가하는것
+    private Map<String, Map> incidentTypeMap;
 
     //생성자에서 사고 타입을 초기화 하기 위해 생성자 주입 방식을 사용
     @Autowired
     public IncidentTypeComponent(EntityManager em) {
         this.em = em;
         list = findAll().orElseThrow(() ->new NoSuchElementException("IncidentType을 찾을 수 없습니다."));
-        incidentTypeList = Collections.unmodifiableList(list);//변경할 수 없는 리스트로 설정
+        incidentTypeMap = new LinkedHashMap<>();
+        for (IncidentType incidentType : list) {
+            //정렬된 채로 리스트에 담기므로 LinkedHashMap, LinkedList 사용
+            if (!incidentTypeMap.containsKey(incidentType.getLarge())) {
+                incidentTypeMap.put(incidentType.getLarge(), new LinkedHashMap<String, List<JSONObject>>());
+            }
+            Map<String, List<JSONObject>> mediumMap = incidentTypeMap.get(incidentType.getLarge());
+            if (!mediumMap.containsKey(incidentType.getMedium())) {
+                mediumMap.put(incidentType.getMedium(), new LinkedList<JSONObject>());
+            }
+            List<JSONObject> jsonObjectList = mediumMap.get(incidentType.getMedium());
+            
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("id", incidentType.getId());
+            jsonObject.put("name", incidentType.getSmall());
+            jsonObjectList.add(jsonObject);
+        }
     }
 
     private Optional<List<IncidentType>> findAll() {
@@ -30,8 +46,8 @@ public class IncidentTypeComponent {
         return Optional.ofNullable(em.createQuery("select i from IncidentType i order by i.large, i.medium, i.small asc", IncidentType.class).getResultList());
     }
 
-    public List<IncidentType> getIncidentTypeList() {
-        return this.incidentTypeList;
+    public Map<String, Map> getIncidentTypeMap() {
+        return incidentTypeMap;
     }
     
 }
